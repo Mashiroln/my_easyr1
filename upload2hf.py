de90f8aa761091e66f6822123e3bac0e816bfe61
Mashiroln <chencanyu@buaa.edu.cn>
2026-02-26 13:56:51 +0000

    [feat](remove sensitive info)

--- upload2hf.py
+++ upload2hf.py
@@ -7,5 +7,3 @@ login()
 upload_folder(folder_path="./checkpoints/easy_r1/qwen2_5_vl_3b_navsim_adas_3x_1k/global_step_150/actor", 
               repo_id="MashiroLn/Curious-VLA", 
               repo_type="model")
-
-# TOKEN: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
